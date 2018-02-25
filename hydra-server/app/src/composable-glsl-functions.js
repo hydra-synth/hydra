978669e271548c17e1851c438effab875e5906a5
ojack <ojackson@gmail.com>
2018-02-25 14:32:21 -0500

    more transforms

--- hydra-server/app/src/composable-glsl-functions.js
+++ hydra-server/app/src/composable-glsl-functions.js
@@ -37,7 +37,6 @@ module.exports = {
       return texture2D(_tex, _st);
     }`
   },
-
   rotate: {
     type: "coord",
     inputs: [
@@ -77,6 +76,66 @@ module.exports = {
             return amount*c0 + (1.0-amount)*c1;
           }`
   },
+  layer: {
+    type: 'combine',
+    inputs: [
+      {
+        name: 'color',
+        type: 'vec4'
+      }
+    ],
+    glsl: `vec4 layer(vec4 c0, vec4 c1){
+        return vec4(mix(c0.rgb, c1.rgb, c1.a), c0.a+c1.a);
+    }
+    `
+  },
+  blend: {
+    type: 'combine',
+    inputs: [
+      {
+        name: 'color',
+        type: 'vec4'
+      },
+      {
+        name: 'amount',
+        type: 'float',
+        default: 0.5
+      }
+    ],
+    glsl: `vec4 blend(vec4 c0, vec4 c1, float amount){
+      return c0*(1.0-amount)+c1*amount;
+    }`
+  },
+  mult: {
+    type: 'combine',
+    inputs: [
+      {
+        name: 'color',
+        type: 'vec4'
+      },
+      {
+        name: 'amount',
+        type: 'float',
+        default: 1.0
+      }
+    ],
+    glsl: `vec4 mult(vec4 c0, vec4 c1, float amount){
+      return c0*(1.0-amount)+(c0*c1)*amount;
+    }`
+  },
+  diff: {
+    type: 'combine',
+    inputs: [
+      {
+        name: 'color',
+        type: 'vec4'
+      }
+    ],
+    glsl: `vec4 diff(vec4 c0, vec4 c1){
+      return vec4(abs(c0.rgb-c1.rgb), max(c0.a, c1.a));
+    }
+    `
+  },
   modulate: {
     type: "combineCoord",
     inputs: [
@@ -93,5 +152,38 @@ module.exports = {
     glsl: `vec2 modulate(vec2 st, vec4 c1, float amount){
             return st+c1.xy*amount;
           }`
+  },
+  invert: {
+    type: 'color',
+    inputs: [],
+    glsl: `vec4 invert(vec4 c0){
+      return vec4(1.0-c0.rgb, c0.a);
+    }`
+  },
+  luminance: {
+    type: 'util',
+    glsl: `float luminance(vec3 rgb){
+      const vec3 W = vec3(0.2125, 0.7154, 0.0721);
+      return dot(rgb, W);
+    }`
+  },
+  luma: {
+    type: 'color',
+    inputs: [
+      {
+        name: 'threshold',
+        type: 'float',
+        default: 0.5
+      },
+      {
+        name: 'tolerance',
+        type: 'float',
+        default: 0.1
+      }
+    ],
+    glsl: `vec4 luma(vec4 c0, float threshold, float tolerance){
+      float a = smoothstep(threshold-tolerance, threshold+tolerance, luminance(c0.rgb));
+      return vec4(c0.rgb*a, a);
+    }`
   }
 }
