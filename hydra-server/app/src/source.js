10851fc51354fa0ce4a5ff9c916dda731ff659de
OLIJAC <ojackson@gmail.com>
2018-03-21 11:51:23 -0500

    video textures fill screen

--- hydra-server/app/src/source.js
+++ hydra-server/app/src/source.js
@@ -6,7 +6,7 @@ var Source = function (opts) {
   this.video = null
   this.tex = this.regl.texture({
   //  flipY: true
-    shape: [1280, 720]
+    shape: [640, 480]
   })
   this.pb = opts.pb
 }
@@ -41,13 +41,13 @@ Source.prototype.init = function (opts) {
   if (opts.type === 'cam') {
     Webcam().then(function (response) {
       self.video = response.video
-    //  self.tex = self.regl.texture(self.video)
+      self.tex = self.regl.texture(self.video)
     //  console.log("received camera input", self)
     })
   } else if (opts.type === 'screen') {
     Screen().then(function (response) {
       self.video = response.video
-    //  self.tex = self.regl.texture(self.video)
+      self.tex = self.regl.texture(self.video)
     //  console.log("received screen input")
     })
   //  to do: delete stream
@@ -59,7 +59,7 @@ Source.prototype.init = function (opts) {
       video.srcObject = opts.stream
       video.addEventListener('loadedmetadata', () => {
         self.video = video
-      //  self.tex = self.regl.texture(self.video)
+        self.tex = self.regl.texture(self.video)
       })
     }
   }
