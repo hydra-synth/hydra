6ba49c15f09171f49bd73689f467d108240b240d
Olivia Jack <ojackson@gmail.com>
2018-07-28 13:06:36 -0500

    updated

---
+++ hydra-server/app/src/video-recorder.js
@@ -0,0 +1,76 @@
+class VideoRecorder {
+  constructor(stream) {
+    this.mediaSource = new MediaSource()
+    this.stream = stream
+
+    let self = this
+    this.mediaSource.addEventListener('sourceopen', () => {
+      console.log('MediaSource opened');
+      self.sourceBuffer = self.mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+      console.log('Source buffer: ', sourceBuffer);
+    })
+  }
+
+  start() {
+    let options = {mimeType: 'video/webm'};
+    this.recordedBlobs = []
+    try {
+     this.mediaRecorder = new MediaRecorder(this.stream, options)
+    } catch (e0) {
+     console.log('Unable to create MediaRecorder with options Object: ', e0)
+     try {
+       options = {mimeType: 'video/webm,codecs=vp9'}
+       this.mediaRecorder = new MediaRecorder(this.stream, options)
+     } catch (e1) {
+       console.log('Unable to create MediaRecorder with options Object: ', e1)
+       try {
+         options = 'video/vp8' // Chrome 47
+         this.mediaRecorder = new MediaRecorder(this.stream, options)
+       } catch (e2) {
+         alert('MediaRecorder is not supported by this browser.\n\n' +
+           'Try Firefox 29 or later, or Chrome 47 or later, ' +
+           'with Enable experimental Web Platform features enabled from chrome://flags.')
+         console.error('Exception while creating MediaRecorder:', e2)
+         return
+       }
+     }
+   }
+   console.log('Created MediaRecorder', this.mediaRecorder, 'with options', options);
+   this.mediaRecorder.onstop = this._handleStop.bind(this)
+   this.mediaRecorder.ondataavailable = this._handleDataAvailable.bind(this)
+   this.mediaRecorder.start(100) // collect 100ms of data
+   console.log('MediaRecorder started', this.mediaRecorder)
+ }
+
+   stop(){
+     this.mediaRecorder.stop()
+   }
+
+ _handleStop() {
+   //const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'})
+   const blob = new Blob(this.recordedBlobs, {type: 'video/webm'})
+   const url = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.style.display = 'none'
+    a.href = url
+    let d = new Date()
+    a.download = `hydra-${d.getYear()}-${d.getMonth() + 1}-${d.getDate()}-${d.getHours()}.${d.getMinutes()}.${d.getSeconds()}.webm`
+    document.body.appendChild(a)
+    a.click()
+    setTimeout(() => {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 100);
+  }
+
+  _handleDataAvailable(event) {
+    if (event.data && event.data.size > 0) {
+      this.recordedBlobs.push(event.data);
+    }
+  }
+}
+
+
+
+
+module.exports = VideoRecorder
