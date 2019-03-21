264a6f7a5269921d5f35231037cb73ed0aab2388
Jack Armitage <jack.armitage@me.com>
2019-03-21 23:00:46 +0000

    cleanup for PR

--- hydra-server/app/src/canvas.js
+++ hydra-server/app/src/canvas.js
@@ -1,8 +1,8 @@
 
 const Canvas = function (canvasElem) {
   const sizeCanvas = () => {
-    canvasElem.width = 3840;//Math.min(1920, window.innerWidth)
-    canvasElem.height = 2160;//Math.min(1080, window.innerHeight)
+    canvasElem.width = Math.min(1280, window.innerWidth)
+    canvasElem.height = Math.min(720, window.innerHeight)
     canvasElem.style.width = '100%'
     canvasElem.style.height = '100%'
   }
