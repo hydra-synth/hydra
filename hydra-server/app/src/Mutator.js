844bf528498af5b95bb7f7dc10f8e71c6405ee57
jamiefaye <jamie@fentonia.com>
2019-06-07 21:44:12 -0500

    Insert null mutator into Hyrda app

---
+++ hydra-server/app/src/Mutator.js
@@ -0,0 +1,43 @@
+const {Parser} = require("acorn");
+const { generate } = require('astring');
+
+class Mutator {
+
+  constructor(editor) {
+	this.editor = editor;
+  }
+
+  back(inst) {
+	console.log("back");
+  }
+
+  mutate(inst) {
+  	// Get text
+  	let text = this.editor.cm.getValue();
+	// Parse to AST
+	let ast = Parser.parse(ast);
+
+	// Modify the AST.
+
+
+	// Generate JS from AST and set back into CodeMirror editor.
+	let regen = generate(ast);
+	this.editor.cm.setValue(regen);
+
+	// Evaluate the updated expression.
+	this.editor.evalAll((code, error) => {
+		console.log('evaluated', code, error);
+	});
+  }
+
+  modify(inst) {
+	console.log("modify");
+  }
+
+  popMod(inst) {
+	console.log("popMod");
+  }
+
+}
+
+module.exports = Mutator
