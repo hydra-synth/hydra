832b9cd98320b3ab0cba5f157a55cc8e3ae83da6
Jelle Akkerman <jellea@users.noreply.github.com>
2019-12-06 00:01:57 +0100

    add video recorder functionality

--- hydra-server/app/src/menu.js
+++ hydra-server/app/src/menu.js
@@ -6,14 +6,20 @@ class Menu {
     this.sketches = obj.sketches
     this.editor = obj.editor
     this.hydra = obj.hydra
+    this.recorder = undefined
+    this.recordingStartTime = undefined
+    this.recordingInterval = undefined
 
     // variables related to popup window
     this.closeButton = document.getElementById("close-icon")
     this.clearButton =  document.getElementById("clear-icon")
     this.shareButton =  document.getElementById("share-icon")
+    this.recordingTime =  document.getElementById("recording-time")
+    this.recorderButton =  document.getElementById("recorder-icon")
     this.shuffleButton = document.getElementById("shuffle-icon")
     this.editorText = document.getElementsByClassName('CodeMirror-scroll')[0]
 
+    this.recorderButton.onclick = this.recordSketch.bind(this)
     this.shuffleButton.onclick = this.shuffleSketches.bind(this)
     this.shareButton.onclick = this.shareSketch.bind(this)
     this.clearButton.onclick = this.clearAll.bind(this)
@@ -45,6 +51,45 @@ class Menu {
           }, () => this.hideConfirmation() )
         }
       })
+  }
+
+  downloadRecording (e) {
+    var videoData = [ e.data ]
+    var blob = new Blob(videoData, {'type': 'video/webm'})
+    var videoURL = URL.createObjectURL(blob)
+    var link = document.createElement('a')
+    link.href = videoURL
+    link.download = "file.webm"
+    link.click()
+  }
+
+  setTime () {
+    let timeSinceStart = new Date().getTime() - this.recordingStartTime
+    let minutes = ""+Math.floor((timeSinceStart % (1000 * 60 * 60)) / (1000 * 60))
+    let seconds = ""+Math.floor((timeSinceStart % (1000 * 60)) / 1000)
+    this.recordingTime.innerText = minutes.padStart(2,"0")
+      + ":" + seconds.padStart(2, "0")
+  }
+
+  recordSketch (){
+    if (this.recordingStartTime) {
+      this.recordingStartTime = false
+      this.recorderButton.className = "far fa-dot-circle icon"
+      this.recorderButton.style = ""
+      this.recordingTime.innerText = ""
+      clearInterval(this.recordingInterval)
+      this.recorder.stop()
+    } else {
+      this.recordingStartTime = new Date().getTime()
+      this.recordingInterval = setInterval(this.setTime.bind(this),1)
+      this.recorderButton.className = "fas fa-dot-circle icon"
+      this.recorderButton.style = "color:red"
+      let canvas = document.querySelector("canvas")
+      let canvasStream = canvas.captureStream(30)
+      this.recorder = new MediaRecorder(canvasStream, {'mimeType': 'video/webm', 'videoBitsPerSecond': 2500000})
+      this.recorder.ondataavailable = this.downloadRecording
+      this.recorder.start()
+    }
   }
 
   showConfirmation(successCallback, terminateCallback) {
