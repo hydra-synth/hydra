392d903326fd6e215f3947b384403760f101b007
ojack <ojackson@gmail.com>
2020-01-24 00:58:40 -0500

    prevented default behavior on key commands - untested

--- hydra-server/app/keymaps.js
+++ hydra-server/app/keymaps.js
@@ -6,33 +6,39 @@ module.exports = {
 
           // shift - ctrl - enter: evalAll
           if ( e.keyCode === 13) {
+            e.preventDefault()
             repl.eval(editor.getValue())
           }
 
           // shift - ctrl - G: share sketch
           if (e.keyCode === 71) {
+              e.preventDefault()
             menu.shareSketch.bind(menu)
           }
 
           // shift - ctrl - h: toggle editor
           if (e.keyCode === 72) {
+              e.preventDefault()
             editor.toggle()
             log.toggle()
           }
 
           // shift - ctrl - s: screencap
           if (e.keyCode === 83) {
+              e.preventDefault()
             screencap()
           }
         } else {
           // ctrl-enter: evalLine
           if ( e.keyCode === 13) {
+              e.preventDefault()
             console.log('eval line')
             repl.eval(editor.getLine())
           }
         }
         // ctrl - /: toggle comment
         if (e.keyCode === 191) {
+            e.preventDefault()
           editor.cm.toggleComment()
         }
       }
@@ -42,6 +48,7 @@ module.exports = {
       if (e.altKey === true) {
         // alt - enter: evalBlock
         if ( e.keyCode === 13) {
+            e.preventDefault()
           repl.eval(editor.getCurrentBlock().text)
         }
       }
