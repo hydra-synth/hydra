7a3dc99b58626f8f11acc2aa1b4d4eed48be6927
micuat <micuat@gmail.com>
2023-05-21 16:45:40 +0200

    macos command works as well as ctrl

--- frontend/web-editor/src/views/editor/keymaps.js
+++ frontend/web-editor/src/views/editor/keymaps.js
@@ -1,11 +1,19 @@
 module.exports = { 
     'Ctrl-Enter': 'editor:evalLine',
+    'Cmd-Enter': 'editor:evalLine',
     'Ctrl-/': 'editor:toggleComment',
+    'Cmd-/': 'editor:toggleComment',
     'Alt-Enter': 'editor:evalBlock',
     'Shift-Ctrl-Enter': 'editor:evalAll',
+    'Shift-Cmd-Enter': 'editor:evalAll',
     'Shift-Ctrl-G': 'gallery:shareSketch',
+    'Shift-Cmd-G': 'gallery:shareSketch',
     'Shift-Ctrl-F': 'editor:formatCode',
+    'Shift-Cmd-F': 'editor:formatCode',
     'Shift-Ctrl-L': 'gallery:saveToURL',
+    'Shift-Cmd-L': 'gallery:saveToURL',
     'Shift-Ctrl-H': 'hideAll',
-    'Shift-Ctrl-S': 'screencap'
+    'Shift-Cmd-H': 'hideAll',
+    'Shift-Ctrl-S': 'screencap',
+    'Shift-Cmd-S': 'screencap'
 }
