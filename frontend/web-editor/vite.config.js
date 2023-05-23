e270acaec83120cd3d80b04a6286f53d2abe2fc1
micuat <micuat@gmail.com>
2023-05-23 17:20:54 +0200

    switching around global define

--- frontend/web-editor/vite.config.js
+++ frontend/web-editor/vite.config.js
@@ -1,27 +1,38 @@
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [],
-  root: 'src',
-  define: {
-    // fix for hydra-synth
-    global: {
-      window: {}
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const commonConfig = {
+    plugins: [],
+    root: 'src',
+    build: {
+      minify: false,
+      outDir: "../public",
+      commonjsOptions: {
+        format: "cjs"
+      }
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+      strictPort: true,
+      hmr: {
+        clientPort: 443 // Run the websocket server on the SSL port
+      }
     }
-  },
-  build: {
-    minify: false,
-    outDir: "../public",
-    commonjsOptions: {
+  };
+  if (command === 'serve') {
+    return {
+      ...commonConfig,
+      define: {
+        // fix for hydra-synth
+        global: "window"
+      },  
     }
-  },
-  server: {
-    host:"0.0.0.0",
-    port:3000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443 // Run the websocket server on the SSL port
+  } else {
+    // command === 'build'
+    return {
+      ...commonConfig
     }
   }
-});
+})
